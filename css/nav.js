ab5b4d250428a3fc05ac10482d572c3f90801c2b
Tommy <tomasmelesse413@gmail.com>
2025-05-09 09:25:58 +0300

    Responsiveness Fix

--- css/nav.js
+++ css/nav.js
@@ -1,7 +1,13 @@
-// js/nav.js
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
-
-hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
+document.querySelector('.mobile-menu-btn').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle('mobile-menu-active');
+    document.body.classList.toggle('no-scroll');
+  });
+  
+  document.querySelectorAll('.nav-elements a').forEach(link => {
+    link.addEventListener('click', function() {
+      if (document.querySelector('nav').classList.contains('mobile-menu-active')) {
+        document.querySelector('nav').classList.remove('mobile-menu-active');
+        document.body.classList.remove('no-scroll');
+      }
+    });
+  });
